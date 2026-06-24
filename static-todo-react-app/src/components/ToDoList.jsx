8c3844baa3c33b6b40ca773f2b72e94aaae54d36
Riona <rionav02@gmail.com>
2026-06-24 17:04:35 -0500

    Created to-do list component.

---
+++ static-todo-react-app/src/components/ToDoList.jsx
@@ -0,0 +1,11 @@
+export function ToDoList() {
+    return (
+        <div>
+            <ul>
+                <li>Learn React</li>
+                <li>Build a Project</li>
+                <li>Read Documentation</li>
+            </ul>
+        </div>
+    );
+}
